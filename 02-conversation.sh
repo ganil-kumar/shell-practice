58c3dc0687f107f3a7a27ed8654e6201a05b0c35
ganil-kumar <g.anilkumar3852@gmail.com>
2026-02-04 18:43:56 +0530

    conersation ex.

---
+++ 02-conversation.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#simple conversation
+
+echo "Akvik: Hello!, ANIL KUMAR. How are you?"
+echo "Anil Kumar: Hi Akvik, I am fine thanks for asking. How are you doing"
+echo "akvik: doing fine. What are you learning Anil Kumar"
+echo "Anil Kumar: I am learning Shell. What about you?"
