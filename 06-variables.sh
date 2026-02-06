54251b32b50200b197f0611923bd82b61dc2a59e
ganil-kumar <g.anilkumar3852@gmail.com>
2026-02-06 15:14:59 +0530

    actions

---
+++ 06-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#I want a command to be executed and take the output into variable, how to do that?
+#VAR_NAME=$(command)
+
+
+TIMESTAMP=$(date)
+
+echo "Script executed at: $TIMESTAMP"
