7954bfdbb250f41ac779549255fab1c044849131
ganil-kumar <g.anilkumar3852@gmail.com>
2026-02-06 23:31:09 +0530

    data types

--- 08-data-types.sh
+++ 08-data-types.sh
@@ -13,4 +13,4 @@ FRUITS=("Apple" "Pine apple" "Banana" "Guva")
 echo "Fruits are: ${FRUITS[@]}"
 echo "First Fruit is: ${FRUITS[0]}"
 echo "Second Fruit is: ${FRUITA[1]}"
-echo "Third Fruit is: ${FRUITS{[2]}}
+echo "Third Fruit is: ${FRUITS{[2]}}"
