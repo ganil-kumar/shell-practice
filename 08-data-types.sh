cfb21844d353db2540572b210e395c65eb2fa1b6
ganil-kumar <g.anilkumar3852@gmail.com>
2026-02-06 23:28:21 +0530

    data types

--- 08-data-types.sh
+++ 08-data-types.sh
@@ -10,7 +10,7 @@ echo "Sum is: $SUM"
 #Arrays
 FRUITS=("Apple" "Pine apple" "Banana" "Guva")
 
-echo "Fruits are: ${FRUITs[@]}"
+echo "Fruits are: ${FRUITS[@]}"
 echo "First Fruit is: ${FRUITS[0]}"
 echo "Second Fruit is: ${FRUITA[1]}"
 echo "Third Fruit is: ${FRUITS{[2]}}
