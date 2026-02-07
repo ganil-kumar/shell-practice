323aae642367978ba50b30cfeb1fc8c4bfb9c1b2
ganil-kumar <g.anilkumar3852@gmail.com>
2026-02-07 19:36:54 +0530

    intial commit

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -9,26 +9,18 @@ NUMBER=$1
 # -lt: Less than
 # -le: Less than or equal to
 
-# if [[ $NUMBER -gt 28 ]]; then
-#     echo "Given number: $NUMBER is greater than 28"
-# elif [[ $NUMBER -eq 28 ]]; then
-#     echo "Given number: $NUMBER is equal to 28"
-# elif [[ $NUMBER -ne 28 ]]; then
-#     echo "Given number: $NUMBER is not equal to 28"
-# elif [[ $NUMBER -ge 28 ]]; then
-#     echo "Given number: $NUMBER is greater than or equal to 28"
-# elif [[ $NUMBER -le 28 ]]; then
-#     echo "Given number: $NUMBER is less than or equal to 28"
-# else
-#     echo "Given number: $NUMBER is less than 28"
-# fi
-
-# string
-
-TODAY=$1
-if [ $TOday != sunday ]
-	echo "Goto school..🏫"
-
- 
-	echo "Take holiday, enjoy..!🤩"
+if [[ $NUMBER -gt 28 ]]; then
+    echo "Given number: $NUMBER is greater than 28"
+elif [[ $NUMBER -eq 28 ]]; then
+    echo "Given number: $NUMBER is equal to 28"
+elif [[ $NUMBER -ne 28 ]]; then
+    echo "Given number: $NUMBER is not equal to 28"
+elif [[ $NUMBER -ge 28 ]]; then
+    echo "Given number: $NUMBER is greater than or equal to 28"
+elif [[ $NUMBER -le 28 ]]; then
+    echo "Given number: $NUMBER is less than or equal to 28"
+else
+    echo "Given number: $NUMBER is less than 28"
 fi
+
+
