98125d73fa3dc5ef523830527ffe0d7e7dccb150
ganil-kumar <g.anilkumar3852@gmail.com>
2026-02-07 21:58:33 +0530

    intial commit

--- 12-logs.sh
+++ 12-logs.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+
 USERID=$(id -u)
 LOGS_FOLDER="/var/log/shell-script"
-LOG_FILE="/var/log/shell-script/$0.log"
+LOGS_FILE="/var/log/shell-script/$0.log"
 
 if [ $USERID -ne 0 ]; then
-    echo "Please run this script with root user access"
+    echo "Please run this script with root user access" #| tee -a $LOGS_FILE
     exit 1
 fi
 
@@ -13,18 +14,18 @@ mkdir -p $LOGS_FOLDER
 
 VALIDATE(){
     if [ $1 -ne 0 ]; then
-        echo "$2 ... FAILURE"
+        echo "$2 ... FAILURE" #| tee -a $LOGS_FILE
         exit 1
     else
-        echo "$2 ... SUCCESS"
+        echo "$2 ... SUCCESS" #| tee -a $LOGS_FILE
     fi
 }
 
-dnf install nginx -y &>> $LOG_FILE
+dnf install nginx -y &>> $LOGS_FILE
 VALIDATE $? "Installing Nginx"
 
-dnf install mysql -y &>> $LOG_FILE
+dnf install mysql -y &>> $LOGS_FILE
 VALIDATE $? "Installing Mysql"
 
-dnf install nodejs -y &>> $LOG_FILE
+dnf install nodejs -y &>> $LOGS_FILE
 VALIDATE $? "Installing nodejs"
