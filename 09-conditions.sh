3ae55364703b379c1eee0f96c31f6602ca77f15a
ganil-kumar <g.anilkumar3852@gmail.com>
2026-02-07 10:41:18 +0530

    intial commit

---
+++ 09-conditions.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+NUMBER=$1
+
+# -eq: Equal to
+# -ne: Not equal to
+# -gt: Greater than
+# -ge: Greater than or equal to
+# -lt: Less than
+# -le: Less than or equal to
+
+if [ $NUMBER -gt 28 ]; then
+    echo "Given number: $NUMBER is greater than 28"
+elif [ $NUMBER -eq 28 ]; then
+    echo "Given number: $NUMBER is equal to 28"
+elif [ $NUMBER -ne 28 ]; then
+    echo "Given number: $NUMBER is not equal to 28"
+elif [ $NUMBER -ge 28 ]; then
+    echo "Given number: $NUMBER is greater than or equal to"
+elif [ $NUMBER -le 28]; then
+    echo "Given number: $NUMBER is less than or equal to"
+else
+    echo "Given number: $NUMBER is less than 28"
+fi
