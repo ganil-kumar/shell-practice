380874a4688f0bf75bbbf60eb94afef11047d388
ganil-kumar <g.anilkumar3852@gmail.com>
2026-02-08 14:03:29 +0530

    intial commit

---
+++ 16-colors.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#Color codes
+R="\e[31m"      # Red 
+G="\e[32m"      # Green 
+Y="\e[33m"      # Yellow
+N="\e[0m"       # Normal 
+B="\e[34m"      # Blue
+
+echo "$R #Warring..! $Y I'm on my way.."
+echo "$G Make things Ready.! $N sure"
