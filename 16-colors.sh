fd0db401640d8d824ffa73bcc97f1aedb7514b58
ganil-kumar <g.anilkumar3852@gmail.com>
2026-02-08 14:05:07 +0530

    intial commit

--- 16-colors.sh
+++ 16-colors.sh
@@ -7,5 +7,5 @@ Y="\e[33m"      # Yellow
 N="\e[0m"       # Normal 
 B="\e[34m"      # Blue
 
-echo "$R #Warring..! $Y I'm on my way.."
-echo "$G Make things Ready.! $N sure"
+echo -e "$R #Warring..! $Y I'm on my way.."
+echo -e "$G Make things Ready.! $N sure"
