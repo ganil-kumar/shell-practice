ed30f0f7ec9b8c902c44c50de0ce986faba7e3a7
ganil-kumar <g.anilkumar3852@gmail.com>
2026-02-08 13:25:37 +0530

    intstall pkg

--- 14-loops.sh
+++ 14-loops.sh
@@ -20,7 +20,7 @@ VALIDATE(){
     fi
 }
 
-for package in $@   # script call - sudo sh 14-loops.sh nginx mysql nodejs etc.. 
+for package in $@   # script call -> sudo sh 14-loops.sh nginx mysql nodejs etc..
 do
     dnf install $package -y &>>$LOGS_FILE
     VALIDATE $? "$package installation"
